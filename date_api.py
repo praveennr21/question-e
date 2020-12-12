3100414be4e2cb5ab2d54a4d00617ede2541be15
praveennr21 <75913055+praveennr21@users.noreply.github.com>
2020-12-12 17:42:52 -0500

    intial

---
+++ date_api.py
@@ -0,0 +1,16 @@
+from flask import Flask,jsonify
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+@app.route('/date', methods=['GET'])
+def get_date():
+    i = datetime.now()
+    tz = pytz.timezone('US/Eastern')
+    date = tz.localize(i)
+
+    return jsonify({'date': date.strftime("%a %b %d %H:%M:%S %Z %Y")})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
